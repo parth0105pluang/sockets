271105bd43a1c94ea415f5af5641c4a31dcd4f53
Parth Sharma <parth.sharma@pluang.com>
2022-02-19 21:42:45 +0530

    added socket.io bs

--- server.js
+++ server.js
@@ -2,7 +2,43 @@ var PORT = process.env.PORT || 3000;
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
+var io = require("socket.io")(http);
 app.use(express.static(__dirname+"/public"));
+io.on("connection",function(){
+    console.log("User connected via socket.io");
+});
 http.listen(PORT,function(){
      console.log("Server Started");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});*/
